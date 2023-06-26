b525458121b29495d504de914059075b11af527b
mars19960000 <marselmustafin19966@mail.ru>
2023-06-26 21:15:00 +0500

    добавил кнопку для рестарта

--- src/Assets/Scripts/Menu/DeadPanel.cs
+++ src/Assets/Scripts/Menu/DeadPanel.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Meny
 {
     public class DeadPanel : CommonPanel
     {
+        public void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
